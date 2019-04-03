5e1466b9321df662ae383314bebc4eb1c61f7598
itapechang <itapechang@163.com>
2019-04-03 23:30:58 +0800

    ribbon规则配置

---
+++ microservice/07consumer-eureka-ribbon-config/src/main/java/com/zcp/springcloud/ConsumerApp.java
@@ -0,0 +1,33 @@
+package com.zcp.springcloud;
+
+import com.zcp.config.RibbonConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author zcp
+ * @create 2019/3/27
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+//给多个服务开启负载均衡@RibbonClients(@RibbonClient("04provider-eureka"),@RibbonClient("04provider-eureka"))
+@RibbonClient(value = "04provider-eureka",configuration = RibbonConfig.class) //ribbon规则(由轮询交叉变换，变为随机负载)
+public class ConsumerApp {
+
+    @Bean
+    @LoadBalanced//给当前对象开启负载均衡，具体是在调用@RibbonClient("04provider-eureka")这个配置设置的服务的时候开启
+    public RestTemplate getTemp(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(ConsumerApp.class,args);
+    }
+}
+
